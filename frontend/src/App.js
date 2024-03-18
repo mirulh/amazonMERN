3db530a3f60161436f81412b00895ae1ceaebeb9
mirulh <amirulhaqem122@gmail.com.com>
2024-03-18 15:41:45 +0800

    created sidebar to filter products by categories and added a search bar. Both of these feature are similar in concept. Need to add search result screen for the next tasks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
@@ -10,7 +10,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Badge from 'react-bootstrap/Badge';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
@@ -20,6 +20,11 @@ import PaymentMetohdScreen from './screens/PaymentMetohdScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderHistoryScreen from './screens/OrderHistoryScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import OrderScreen from './screens/OrderScreen';
+import Button from 'react-bootstrap/Button';
+import { getError } from './utils';
+import axios from 'axios';
+import SearchBox from './components/SearchBox';
 // [3]
 import UseState from './testOnly/UseState';
 import UseEffect from './testOnly/UseEffect';
@@ -31,7 +36,6 @@ import UseContext from './testOnly/UseContext';
 import UseContext2 from './testOnly/UseContext2';
 import UseContext3 from './testOnly/UseContext3';
 import UseContext4 from './testOnly/UseContext4';
-import OrderScreen from './screens/OrderScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -41,15 +45,36 @@ function App() {
     ctxDispatch({ type: 'USER_SIGNOUT' });
     localStorage.removeItem('userInfo');
     localStorage.removeItem('shippingAddress');
-    localStorage.removeItem('cartItems');
+    // localStorage.removeItem('cartItems'); // i added this
     localStorage.removeItem('paymentMethod');
     // prevent userInfo = null, error
     window.location.href = '/signin';
   };
 
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const { data } = await axios.get(`/api/products/categories`);
+        setCategories(data);
+      } catch (err) {
+        toast.error(getError(err));
+      }
+    };
+    fetchCategories();
+  }, []);
+
   return (
     <BrowserRouter>
-      <div className="d-flex flex-column site-container">
+      <div
+        className={
+          sidebarIsOpen
+            ? 'd-flex flex-column site-container active-cont'
+            : 'd-flex flex-column site-container'
+        }
+      >
         {/* [4] */}
         <ToastContainer position="bottom-center" limit={1} />
         <header>
@@ -57,6 +82,12 @@ function App() {
           {/* [7] */}
           <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
+              <Button
+                variant="dark"
+                onClick={() => setSidebarIsOpen(!sidebarIsOpen)}
+              >
+                <i className="fas fa-bars"></i>
+              </Button>
               <LinkContainer to="/">
                 <Navbar.Brand>amazona</Navbar.Brand>
               </LinkContainer>
@@ -64,6 +95,7 @@ function App() {
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               {/* [6] */}
               <Navbar.Collapse id="basic-navbar-nav">
+                <SearchBox />
                 <Nav className="me-auto w-100 justify-content-end">
                   <Link to="/cart" className="nav-link">
                     Cart
@@ -101,6 +133,29 @@ function App() {
             </Container>
           </Navbar>
         </header>
+        <div
+          className={
+            sidebarIsOpen
+              ? 'active-nav side-navbar d-flex justify-content-between flex-wrap flex-column'
+              : 'side-navbar d-flex justify-content-between flex-wrap flex-column'
+          }
+        >
+          <Nav className="flex-column text-white w-100 p-2">
+            <Nav.Item>
+              <strong>Categories</strong>
+            </Nav.Item>
+            {categories.map((category) => (
+              <Nav.Item key={category}>
+                <LinkContainer
+                  to={`/search/category=${category}`}
+                  onClick={() => setSidebarIsOpen(false)}
+                >
+                  <Nav.Link>{category}</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+            ))}
+          </Nav>
+        </div>
         {/* main section  */}
         <main>
           <Container className="mt-3">
---
+++ frontend/src/components/SearchBox.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
+
+export default function SearchBox() {
+  const navigate = useNavigate();
+  const [query, setQuery] = useState('');
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate(query ? `/search/?query=${query}` : '/search');
+  };
+
+  return (
+    <Form className="d-flex me-auto" onSubmit={submitHandler}>
+      <InputGroup>
+        <Form.Control
+          type="text"
+          name="q"
+          id="q"
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="search products..."
+          // [1]
+          aria-label="Search Products"
+          aria-describedby="button-search"
+        ></Form.Control>
+        <Button variant="outline-primary" type="submit" id="button-search">
+          <i className="fas fa-search"></i>
+        </Button>
+      </InputGroup>
+    </Form>
+  );
+}
+
+/* 
+
+[1] aria-label/aria-describedby - for screen reader
+
+
+*/
