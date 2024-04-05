51d866c8950b90f06b60a079d38824161b8cbcdf
mirulh <amirulhaqem122@gmail.com.com>
2024-04-05 16:20:40 +0800

    44) completed list orders feature for admin. Admin will be able to view all orders made and their details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -42,6 +42,7 @@ import DashboardScreen from './screens/DashboardScreen';
 import AdminRoute from './components/AdminRoute';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
+import OrderListScreen from './screens/OrderListScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -228,6 +229,14 @@ function App() {
                 element={
                   <AdminRoute>
                     <ProductListScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+              <Route
+                path="/admin/orders"
+                element={
+                  <AdminRoute>
+                    <OrderListScreen />
                   </AdminRoute>
                 }
               ></Route>
